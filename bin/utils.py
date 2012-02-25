a015812aa56ca677ced6b03a9c8868e2e7a8b8d7
straup <straup@gmail.com>
2012-02-25 00:42:54 -0800

    add bin dir and tools to export objects as JSON files; add JSON files; ignore ~ files

---
+++ bin/utils.py
@@ -0,0 +1,13 @@
+def id2path(id):
+
+    tmp = str(id)
+    parts = []
+
+    while len(tmp) > 3:
+        parts.append(tmp[0:3])
+        tmp = tmp[3:]
+
+    if len(tmp):
+        parts.append(tmp)
+
+    return "/".join(parts)
